21aea464bcccec8f0b2286cc26131c1aa3732f6c
Felix <fluffythelegacy@gmail.com>
2025-04-13 12:28:49 +0300

    Delete add_category_field.sql

--- add_category_field.sql
+++
@@ -1,5 +0,0 @@
--- Добавление поля category
-ALTER TABLE "product" ADD COLUMN "category" character varying DEFAULT 'Прочее';
-
--- Добавление поля marketplace 
-ALTER TABLE "product" ADD COLUMN "marketplace" character varying; 
